b489e531190b7759c6b2a4b93d3ab9f34a0d3c97
Melih Sünbül <67157758+melihsunbul@users.noreply.github.com>
2024-05-20 21:20:25 +0200

    added retry logic

--- django_sharepoint_storage/SharePointStorage.py
+++ django_sharepoint_storage/SharePointStorage.py
@@ -19,11 +19,16 @@ class SharePointStorage(Storage):
         super().__init__(*args, **kwargs)
         self.location = location
 
+    @classmethod
+    @staticmethod
+    def print_failure(retry_number, ex):
+        Logger.logger_msg(f"{retry_number}: {ex}")
+
     def _open(self, name, mode='rb'):
         from django_sharepoint_storage.SharePointCloudStorageUtils import get_server_relative_path
         if mode in ['r', 'rb']:
             file_url = self.url(name)
-            file = ctx.web.get_file_by_server_relative_path(get_server_relative_path(file_url)).execute_query()
+            file = ctx.web.get_file_by_server_relative_path(get_server_relative_path(file_url)).execute_query_retry(max_retry=5, timeout_secs=5, failure_callback=SharePointStorage.print_failure)
             binary_file = file.open_binary(ctx, get_server_relative_path(file_url))
             bytesio_object = BytesIO(binary_file.content)
             return bytesio_object
@@ -35,17 +40,17 @@ class SharePointStorage(Storage):
     def _save(self, name, content):
         # Here, name will have the format 'upload_to/filename.ext'
         folder_path = f"Shared Documents/{os.getenv('DEPLOYMENT_ENVIRONMENT', 'LOCAL')}-{os.getenv('K8S_NAMESPACE', 'ENV')}/{os.getenv('KEYCLOAK_INTERNAL_CLIENT_ID', 'Local')}/{os.getenv('INSTANCE_RESOURCE_IDENTIFIER', f'{platform.node()}/{DB_NAME}')}/{self.location}/{os.path.dirname(name)}"
-        target_folder = ctx.web.ensure_folder_path(folder_path).get().select(["ServerRelativePath"]).execute_query()
+        target_folder = ctx.web.ensure_folder_path(folder_path).get().select(["ServerRelativePath"]).execute_query_retry(max_retry=5, timeout_secs=5, failure_callback=SharePointStorage.print_failure)
 
         file_content = content.read()
 
-        target_folder.upload_file(os.path.basename(name), file_content).execute_query()
+        target_folder.upload_file(os.path.basename(name), file_content).execute_query_retry(max_retry=5, timeout_secs=5, failure_callback=SharePointStorage.print_failure)
 
         return name
 
     def delete(self, name):
-        file = File.from_url(self.url(name)).with_credentials(client_credentials).execute_query()
-        file.delete_object().execute_query()
+        file = File.from_url(self.url(name)).with_credentials(client_credentials).execute_query_retry(max_retry=5, timeout_secs=5, failure_callback=SharePointStorage.print_failure)
+        file.delete_object().execute_query_retry(max_retry=5, timeout_secs=5, failure_callback=SharePointStorage.print_failure)
 
     def exists(self, name):
         return False
